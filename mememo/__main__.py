0164f7d1019e0584e22c7b888d2f6a21a07707d7
scr1p7k177y <scriptkitty@walang.studio>
2026-03-01 22:07:35 +0800

    fix linter/ruff issues

--- mememo/__main__.py
+++ mememo/__main__.py
@@ -7,7 +7,8 @@ Usage:
 """
 
 import argparse
-from importlib.metadata import PackageNotFoundError, version as _pkg_version
+from importlib.metadata import PackageNotFoundError
+from importlib.metadata import version as _pkg_version
 
 try:
     __version__ = _pkg_version("mememo")
